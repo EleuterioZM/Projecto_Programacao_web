49c4915457d647c2b4ee5a1621c05d6595f6eeb9
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2024-05-02 13:07:56 +0200

    #First Comiit

---
+++ cursos.php
@@ -0,0 +1,276 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  </head>
+  
+  
+  
+  <body >
+  <div>
+  <div class="offcanvas offcanvas-start show" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
+  <div class="offcanvas-header" style="background:rgba(201, 62, 12, 0.7);">
+    <h5 class="offcanvas-title" id="offcanvasLabel">Sistema de gestao de matriculas</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+   <p>Bem-vindo(a) ao revolucionário Sistema de Gestão de Matrículas!
+Em um mundo cada vez mais conectado e tecnológico, a eficiência e a praticidade são essenciais para o suces
+so de qualquer instituição de ensino. Reconhecendo essa necessidade,
+ desenvolvemos um sistema que simplificará o processo de matrículas, permitindo que você concentre seu
+ s esforços em oferecer uma educação de qualidade.</p>
+   
+
+  </div>
+
+</div>
+</div>
+
+
+      <header style="margin-left:-15px;">
+          
+
+
+  
+
+      </header>
+  
+  
+	<section>
+	<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner" id="slide">
+    <div class="carousel-item active">
+      <img src="img/undraw_product_iteration_kjok.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/undraw_Sign_up_n6im.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/undraw_Reminder_re_fe15.png" class="d-block w-100" alt="...">
+    </div>
+	
+	<div class="carousel-item">
+      <img src="img/undraw_Code_review_re_woeb.png" class="d-block w-100" alt="...">
+    </div>
+	<div class="carousel-item">
+      <img src="img/undraw_Programming_re_kg9v.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+
+
+
+<div style="margin-left:30px;">
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card" id="card">
+   <a href="FormularioMatricula.html">   <img src="img/iscim.jpg" class="card-img-top" alt="..." height="120px;" width="50px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">Marketing</h5>
+        <p class="card-text">O marketing é a chave para o sucesso nos negócios modernos. Ele é a ponte que conecta você aos seus clientes...</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" id="card1">
+    <a href="FormularioMatricula.html">  <img src="img/iscim1.jpg" class="card-img-top" alt="..."  height="110px;" width="20px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">INFORMÁTICA DE SISTEMA</h5>
+        <p class="card-text">onjunto de programas que gerenciam recursos, processadores, armazenamento, dispositivos de E/S.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" id="card2">
+    <a href="FormularioMatricula.html">  <img src="img/iscim2.jpg" class="card-img-top" alt="..."  height="120px;" width="50px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">INFORMÁTICA DE GESTÃO</h5>
+        <p class="card-text">é uma formação integrada numa área em constante evolução.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" id="card3">
+   <a href="FormularioMatricula.html">   <img src="img/iscim3.jpg" class="card-img-top" alt="..."  height="110px;" width="20px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">DIREITO</h5>
+        <p class="card-text">é a disciplina acadêmica que estuda o conjunto de leis e regulamentos.... </p>
+      </div>
+    </div>
+  </div>
+   <div class="col">
+    <div class="card" id="card4">
+    <a href="FormularioMatricula.html">  <img src="img/it.png" class="card-img-top" alt="..."  height="120px;" width="30px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">GESTÃO EMPRESARIAL</h5>
+        <p class="card-text">Planejamento e estratégias que visam alcançar melhores resultados no...</p>
+      </div>
+    </div>
+  </div>
+   <div class="col">
+    <div class="card" id="card5">
+     <a href="FormularioMatricula.html"> <img src="img/designer.jpg" class="card-img-top" alt="..."  height="120px;" width="30px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">MULTIMÉDIA</h5>
+        <p class="card-text"> A formação em Multimídia combina conhecimentos de diversas áreas diferentes, como Tecnologia e Artes ...</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div id="paragrafo">
+<h2 style="margin-left:65vw; margin-top:-33vw; color:tomato;">Apresentamos os nossos cursos relacionados com tecnologia</h2>
+<hr style="margin-left:75vw; margin-right:5vw; height:12px; color:green; text-size:12px;">
+<hr style="margin-left:75vw; margin-right:5vw; height:12px; color:green">
+
+<p style="margin-left:62vw;">O seu sonho é trabalhar com tecnologia?<br> Então, temos uma ótima notícia: existem vários cursos de graduação em alta que abrangem esse tema e oferecem ótimas oportunidades de trabalho e carreira. Isso porque esse mercado está em expansão desde o início da Transformação Digital.
+
+Nesse contexto, é possível sentir o impacto do desenvolvimento tecnológico em variados campos.<br><br> E nada mais justo do que aproveitar essa onda de oportunidade no momento de ingressar na faculdade.
+
+Pensando em auxiliar você a tomar uma boa decisão, elaboramos este post para apresentar as principais graduações para quem quer trabalhar com tecnologia. Confira!</p>
+</div>
+<div style=" height: 3px;
+  background:rgba(221,68,12,0.7);; 
+  width: 50%; margin-top:7vw;"></div>
+  
+  
+  
+  <!--cursos-->
+  <div>
+  <div style="margin-left:30px; margin-top:2vw;">
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card" id="card">
+     <a href="#"> <img src="img/forex.jpg" class="card-img-top" alt="..." height="120px;" width="50px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">GESTÃO DE EMPRESAS</h5>
+        <p class="card-text">processo fundamentado em planejamento e estratégias que visam alcançar melhores resultados para o...</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" id="card1">
+     <a href="#"> <img src="img/cibernetica.jpg" class="card-img-top" alt="..."  height="110px;" width="20px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">INFORMÁTICA DE SISTEMA</h5>
+        <p class="card-text">onjunto de programas que gerenciam recursos, processadores, armazenamento, dispositivos de E/S.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card" id="card2">
+    <a href="#">  <img src="img/marketing.png" class="card-img-top" alt="..."  height="120px;" width="50px;"></a>
+      <div class="card-body">
+        <h5 class="card-title">INFORMÁTICA DE GESTÃO</h5>
+        <p class="card-text">é uma formação integrada numa área em constante evolução.</p>
+      </div>
+    </div>
+ 
+</div>
+</div>
+<div style="  width: 5px; 
+  background:rgba(240,128,128); 
+  height: 190px; 
+  transition: height 0.3s; margin-left:60vw; margin-top:-15vw;"></div>
+  <legend  style="margin-left:63vw; margin-top:-13vw; color:rgba(255,69,0);" >Por apenas um click na imagem e ja esta!</legend>
+  <p >
+  <ul style="margin-left:60vw; margin-top:-10vw;">
+ <li style="color:green;"> Siga os passos para fazer a sua incricao:</li>
+ <br>
+ <li> 1-Va ate a imagem do curso quer se matricular e da um click</li>
+  <li>2-Em seguida preencha os campos da matricula</li>
+  <li>3-Concorda com todos os termos</li>
+  <li>4-Envia o formulario e ja esta!!</li>
+  </ul>
+  </p>
+  </div>
+	</section>
+
+
+  
+  <footer class="footer" style="height: 14vw; margin-top:3vw;height: 18vw;">
+  
+     <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>company</h4>
+          <ul>
+            <li><a href="#">about us</a></li>
+            <li><a href="#">our services</a></li>
+          
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>get help</h4>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">shipping</a></li>
+          
+            
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>online shop</h4>
+          <ul>
+            <li><a href="#">watch</a></li>
+            <li><a href="#">bag</a></li>
+           
+          
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>follow us</h4>
+          <div class="social-links">
+            <a href="https://www.facebook.com/eleuteriozacarias.mabecuane?mibextid=LQQJ4d"><i class="fab fa-facebook-f"></i></a>
+         
+            <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+            
+          <a href="https://wa.me/+258844318136"><i class="fab fa-whatsapp"></i></a>
+          </div>
+        </div>
+      </div>
+     </div>
+	 <div style="margin-top:87px;">
+	 
+	 
+	 
+	 
+	 <nav aria-label="...">
+  <ul class="pagination pagination-sm">
+    <li class="page-item active" aria-current="page">
+      <span class="page-link">1</span>
+    </li>
+    <li class="page-item"><a class="page-link" href="Home.php">2</a></li>
+    <li class="page-item"><a class="page-link" href="FormularioMatriculas.php">3</a></li>
+  </ul>
+</nav>
+	 
+	 
+	 
+	 
+	 
+	 </div>
+  </footer>
+<div>
+<nav style="background:rgba(221,68,12,0.7); height:3vw;padding:12px; font-style:arial;">
+<ul>
+<li style=" margin-left:12vw;">Todos direitos reservados ao Eleuterio Zacarias Mabecuane <a href="#">
+
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-outbound-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z"/>
+</svg>
+
+</a></li>
+</ul>
+</nav>
+</div>
+  
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
